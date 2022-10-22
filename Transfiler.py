7390bebf09ec852be8c8a1d21cac86c44daa3f34
이석호 <mercen@mercen.net>
2022-10-22 09:45:11 +0900

    ADD: COMPLETED

--- Transfiler.py
+++ Transfiler.py
@@ -59,60 +59,111 @@ class BicycleIO:
         except:
             self.Exception('출력에 실패했습니다.')
 
-IO = BicycleIO()
-CODE = '# Transfiled from Bicycle\n'
+def Transfile(original):
+    FUNC = {}
+    CODE = ''
+    for line in original.splitlines():
 
-for line in IO.Read().splitlines():
+        nil = (len(line) - len(line.lstrip()))
 
-    nil = (len(line) - len(line.lstrip()))
+        # Parsing
+        cmd = split(line, posix = False)
+        for idx, item in enumerate(cmd):
+            if item[0] in ['\'', '"']:
+                item = sub(r"(\\\()", "{", item)
+                cmd[idx] = 'f' + sub(r"[\)]", "}", item)
+            if item in list(FUNC.keys()):
+                param = []
+                for inner in cmd[idx + 1:]:
+                    if inner.endswith(','):
+                        param.append(inner[:-1])
+                    else:
+                        if param: param.append(inner)
+                        break
+                if len(param) == FUNC[item]:
+                    for params in param:
+                        for items in cmd:
+                            if params in items: cmd.remove(items)
+                    cmd[idx] = f'{item}({",".join(param)})'
+                elif not param:
+                    try:
+                        exec(f'def a(t): print(t); a({cmd[idx + 1]})')
+                        cmd[idx] = f'{item}({cmd.pop(idx + 1)})'
+                    except:
+                        cmd[idx] = f'{item}()'
 
-    # Parsing
-    cmd = split(line, posix = False)
-    for idx, item in enumerate(cmd):
-        if item[0] in ['\'', '"']:
-            item = sub(r"(\\\()", "{", item)
-            cmd[idx] = 'f' + sub(r"[\)]", "}", item)
+        # Removing Exception
+        if not cmd:
+            out = line
+            continue
 
-    # Removing Exception
-    if not cmd:
-        out = line
-        continue
+        # Print Function
+        if cmd[-1] in ['출력', '말하기']:
+            out = f'print({" ".join(cmd[:-1])})'
+        
+        # Conditional
+        elif cmd[0] in ['만약', '만약에', '아니고', '또는', '아니라면:', '아니면:']:
 
-    # Print Function
-    if cmd[-1] in ['출력', '말하기']:
-        out = f'print({" ".join(cmd[:-1])})'
+            if cmd[-1].endswith('라면:'):
+                if cmd[-1][-4] == '이':
+                    outTrim = 4
+                elif cmd[-1][-5] + cmd[-1][-4] == '아니':
+                    outTrim = 5
+                else: outTrim = 3
+                cmd[-1] = cmd[-1][:-outTrim] + ':'
+
+            if cmd[0] in ['만약', '만약에']:
+                out = f'if {" ".join(cmd[1:])}'
+
+            elif cmd[0] in ['아니고', '또는']:
+                out = f'elif {" ".join(cmd[1:])}'
+
+            else: out = 'else:'
+
+        # Function
+        elif cmd[0][-1] + cmd[-1][-1] == '::':
+            if len(cmd[1:]) == 0:
+                FUNC[cmd[0][:-2]] = len(cmd[1:])
+                out = f'def {cmd[0][:-2]}():'
+            else:
+                FUNC[cmd[0][:-1]] = len(cmd[1:])
+                out = f'def {cmd[0][:-1]}({" ".join(cmd[1:])[:-1]}):'
+        elif cmd[-1] in ['반환', '돌려주기']:
+            out = f'return {" ".join(cmd[:-1])}'
+
+        # For
+        elif cmd[-1].endswith('까지:'):
+            if cmd[-2].endswith('부터'):
+                outRange = cmd[-2][:-2]
+            else: outRange = 0
+            out = f'for {cmd[0][:-1]} in range({outRange}, {cmd[-1][:-3]}):'
+        
+        elif cmd[-1].endswith('로부터:'):
+            if cmd[-1][-5] == '으':
+                outTrim = 5
+            else: outTrim = 4
+            out = f'for {cmd[0][:-1]} in {cmd[-1][:-outTrim]}:'
+
+        elif cmd[-1] == '불러오기':
+            mod = open(cmd[0] + '.byc', encoding = 'UTF-8').read()
+            for item in mod.splitlines():
+                if item.startswith('def'):
+                    fnc = item[4:].split('(')
+                    FUNC[fnc[0]] = len(fnc[1][0].split(')')[0].split(','))
+            CODE = f'\n# Module {cmd[0]}\n{mod}\n# Module End\n{CODE}'
+            continue
+
+        else:
+            CODE += f'\n{" ".join(cmd)}'
+            continue
+
+        CODE += f'\n{nil * " " + out}'
     
-    # Conditional
-    elif cmd[0] in ['만약', '만약에']:
-        out = f'if {" ".join(cmd[1:])}'
+    return CODE
 
-    # Conditional
-    elif cmd[0] in ['아니라면:', '아니면:']:
-        out = 'else:'
+if __name__ == '__main__':
+    IO = BicycleIO()
+    CODE = Transfile(IO.Read())
 
-    # Function
-    elif cmd[-1] in ['반환', '돌려주기']:
-        out = f'return {" ".join(cmd[:-1])}'
-
-    # For
-    elif cmd[-1].endswith('까지:'):
-        if cmd[-2].endswith('부터'):
-            outRange = cmd[-2][:-2]
-        else: outRange = 0
-        out = f'for {cmd[0][:-1]} in range({outRange}, {cmd[-1][:-3]}):'
-    
-    elif cmd[-1].endswith('로부터:'):
-        if cmd[-1][-5] == '으':
-            outTrim = 5
-        else: outTrim = 4
-        out = f'for {cmd[0][:-1]} in {cmd[-1][:-outTrim]}:'
-
-    else:
-        out = line
-
-    CODE += f'\n{nil * " " + out}'
-
-print(CODE)
-
-print('변환을 완료했습니다.\n')
-IO.Write(CODE)
+    print('변환을 완료했습니다.\n')
+    IO.Write('# Transfiled from Bicycle\n' + CODE)
